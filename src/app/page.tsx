2c8e2da43027632324ae208153ac9b8f86daf21e
Callum Bonnyman <callum@wrux.com>
2024-10-17 15:09:18 +0700

    Added ShadCN.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 // import Image from "next/image";
 
 import Link from "next/link";
+import { Button } from "~/components/ui/button";
 import { client } from "~/sanity/lib/client";
 import { POST_TEASER_QUERY } from "~/sanity/lib/queries";
 
@@ -13,13 +14,18 @@ export default async function Home() {
     <div>
       <h1>Homepage</h1>
 
-      {posts.map((post) => (
-        <article key={post._id}>
-          <h2>
-            <Link href={`/posts/${post.slug}`}>{post.title}</Link>
-          </h2>
-        </article>
-      ))}
+      <ul className="space-y-8">
+        {posts.map((post) => (
+          <article key={post._id}>
+            <h2>
+              <Link href={`/posts/${post.slug}`}>{post.title}</Link>
+            </h2>
+            <Button asChild>
+              <Link href={`/posts/${post.slug}`}>View Post</Link>
+            </Button>
+          </article>
+        ))}
+      </ul>
     </div>
   );
 }
