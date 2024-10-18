76d71b246757405002f3f1a5615b930d99120ddd
Callum Bonnyman <callum@wrux.com>
2024-10-18 12:52:00 +0700

    Added simple footer layout.

--- src/app/(content)/layout.tsx
+++ src/app/(content)/layout.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { Badge } from '~/components/ui/badge';
+import { typographyVariants } from '~/components/ui/typography';
 import { client } from '~/sanity/lib/client';
 import { COUNTRY_LIST_QUERY } from '~/sanity/lib/queries';
 
@@ -64,8 +65,38 @@ async function SiteHeader() {
 
 function SiteFooter() {
   return (
-    <footer>
-      <p>BYE</p>
+    <footer className="pb-12 pt-8 lg:pb-20 lg:pt-12">
+      <div className="container max-w-full">
+        <div className="flex items-center gap-4">
+          <Link
+            className="block w-full text-lg md:w-auto lg:text-2xl"
+            href="/"
+            aria-label="Go to homepage"
+          >
+            <div className="circle" aria-hidden="true" user-select="none"></div>
+          </Link>
+
+          <p className={typographyVariants({ variant: 'small' })}>
+            Manhole Gallery &copy;{' '}
+            <a
+              href="https://wrux.com"
+              target="_blank"
+              className="link-underline"
+            >
+              WRUX
+            </a>{' '}
+            and{' '}
+            <a
+              href="https://callum.co.uk"
+              target="_blank"
+              className="link-underline"
+            >
+              Callum Bonnyman
+            </a>{' '}
+            {new Date().getFullYear()}.
+          </p>
+        </div>
+      </div>
     </footer>
   );
 }
