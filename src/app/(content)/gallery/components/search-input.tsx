5e951f0ba0d4173565928fc59d39d9fe51ba8bb3
Callum Bonnyman <callum@wrux.com>
2024-10-28 13:40:23 +0700

    Added icon to search.

--- src/app/(content)/gallery/components/search-input.tsx
+++ src/app/(content)/gallery/components/search-input.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { MagnifyingGlass } from '@phosphor-icons/react';
 import { useEffect, useState } from 'react';
 import { useGalleryContext } from './gallery-provider';
 
@@ -20,9 +21,12 @@ export function SearchInput() {
   }, [searchTerm, query, setQuery]);
 
   return (
-    <div className="flex flex-1">
+    <div className="relative flex flex-1">
+      <span className="pointer-events-none absolute left-0 top-1/2 flex h-10 w-16 -translate-y-1/2 items-center justify-center text-xl">
+        <MagnifyingGlass className="h-[1em] w-[1em]" />
+      </span>
       <input
-        className="focus-visible:ring-highlight flex-1 rounded-full border bg-card px-6 py-2 text-card-foreground shadow-highlight focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-opacity-50"
+        className="focus-visible:ring-highlight flex-1 rounded-full border bg-card py-2 pl-12 pr-6 text-card-foreground shadow-highlight focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-opacity-50"
         type="search"
         placeholder="Search"
         value={searchTerm}
