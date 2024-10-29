e75f407d05b89921f13a35065b3fac97939a67de
Callum Bonnyman <callum@wrux.com>
2024-10-29 14:18:15 +0700

    Fixed logo title text alignment off on mobile.

--- src/components/ui/common/logo-title.tsx
+++ src/components/ui/common/logo-title.tsx
@@ -9,18 +9,14 @@ export type LogoTitleProps = {
 export default function LogoTitle({ title }: LogoTitleProps) {
   return (
     <div className="gap-space flex items-start">
-      <Link
-        className="text-lg lg:text-2xl"
-        href="/"
-        aria-label="Go to homepage"
-      >
+      <Link className="text-lg" href="/" aria-label="Go to homepage">
         <div className="circle" aria-hidden="true" user-select="none"></div>
       </Link>
 
       <h1
         className={cn(
           typographyVariants({ variant: 'h2' }),
-          'mt-0.5 inline-block',
+          'mt-[-0.08em] inline-block',
         )}
       >
         {title}
